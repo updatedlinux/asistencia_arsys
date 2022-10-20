e3d495b5b0a56c69037b42c8f6bb00451470e405
Jonathan Melendez <jmelendez@petro.gob.ve>
2022-10-20 10:51:30 -0400

    Cambios en el front

--- ajax/asistencia.php
+++ ajax/asistencia.php
@@ -27,16 +27,16 @@ switch ($_GET["op"]) {
                 $tipo = "Entrada";
         		$rspta=$asistencia->registrar_entrada($codigo_persona,$tipo);
     			//$movimiento = 0;
-    			echo $rspta ? '<h3><strong>Nombres: </strong> '. $result['nombre'].' '.$result['apellidos'].'</h3><div class="alert alert-success"> Ingreso registrado '.$hora.'</div>' : 'No se pudo registrar el ingreso';
+    			echo $rspta ? '<h3 style="background-color:#FFFFFF;><strong>Nombres: </strong> '. $result['nombre'].' '.$result['apellidos'].'</h3><div class="alert alert-success"> Ingreso registrado '.$hora.'</div>' : 'No se pudo registrar el ingreso';
    		  }else{ 
                 $tipo = "Salida";
          		$rspta=$asistencia->registrar_salida($codigo_persona,$tipo);
      			//$movimiento = 1;
-     			echo $rspta ? '<h3><strong>Nombres: </strong> '. $result['nombre'].' '.$result['apellidos'].'</h3><div class="alert alert-danger"> Salida registrada '.$hora.'</div>' : 'No se pudo registrar la salida';             
+     			echo $rspta ? '<h3 style="background-color:#FFFFFF><strong>Nombres: </strong> '. $result['nombre'].' '.$result['apellidos'].'</h3><div class="alert alert-danger"> Salida registrada '.$hora.'</div>' : 'No se pudo registrar la salida';             
         } 
         } else {
 		         echo '<div class="alert alert-danger">
-                       <i class="icon fa fa-warning"></i> No hay empleado registrado con esa código...!
+                       <i class="icon fa fa-warning"></i> MALDITO PON TU PUTA CEDULA BIEN!
                          </div>';
         }
 
